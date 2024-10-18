d687ae7bd6287e4657a2279d2d0c4b496a286d8b
Sharath-Chandra43 <addanapurisharathchandra99@gmail.com>
2024-10-19 00:59:07 +0530

    updated

---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { LOGO } from '../utils/constants'
+import { AiOutlineUser } from "react-icons/ai";
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+
+const Header = () => {
+  return (
+    <>        
+        <div className='top'>
+            <p className='text'>Free Shipping on orders above 999/-</p>
+            <p className='text'>Call us on: +91 98768 05120</p>
+        </div>
+
+         {/* Nav Bar section */}
+
+    <div className='header'>                             
+        <div className='logo-container'>
+            <img src={LOGO} alt='logo'  className='logo'/>
+            <h1 className='logo-text'>Chaperone</h1>
+        </div>
+        <div>
+        <ul className='nav-container'>
+            <li >Home</li>
+            <li>Plants & Pots</li>
+            <li>Tools ⩡</li>
+            <li>Our Services ⩡</li>
+            <li>Blog</li>
+            <li>Our Story</li>
+            <li>FAQS</li>
+        </ul>
+        </div>
+
+
+        {/*My profile and Cart Section */}
+    
+        
+            <div className='right-section'>
+            <span className='profile-and-cart' >
+                <AiOutlineUser className='icons-size' />
+                My-Profile
+            </span>
+            <span className='profile-and-cart'>
+            <HiOutlineShoppingCart className='icons-size'/>
+                Cart
+            </span>
+            </div>
+        
+
+
+    </div>
+</>
+
+  )
+}
+
+export default Header
